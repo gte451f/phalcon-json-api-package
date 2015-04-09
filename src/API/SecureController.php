11848d71359e169aa964bd1451a6262f6fa138ea
jjenkins <jjenkins@smithcarson.com>
2015-04-08 20:19:54 -0400

    add option to disable/enable security in $config

--- src/API/SecureController.php
+++ src/API/SecureController.php
@@ -4,37 +4,48 @@ namespace PhalconRest\API;
 use \PhalconRest\Util\HTTPException;
 
 /**
- * Same base controller but checks for a valid session before proceeding
+ * Same base controller but checks for a valid token if security is enabled
+ * otherwise it proceeds to the baseController
  */
 class SecureController extends BaseController
 {
 
     public function __construct($parseQueryString = true)
     {
-        $token = $this->request->getHeader("X_AUTHORIZATION");
-        $token = str_ireplace("Token: ", '', $token);
-        $token = trim($token);
-        // $this->auth = $this->getDI()->get('auth');
-        // parent::__construct($parseQueryString);
+        $config = $this->getDI()->get('config');
         
-        if (strlen($token) < 30) {
-            throw new HTTPException("Bad token supplied", 401, array(
-                'internalCode' => '0273497957'
-            ));
-        }
-        
-        // $token = 'f8o92fTk58c22sD2280bfByn0875TN0fcUq8MKGfhrScd2by6JcceuF783ttFadf';
-        // TODO Check for a valid session
-        // Check if the variable is defined
-        if ($this->auth->isLoggedIn($token)) {
-            parent::__construct($parseQueryString);
-            // if($profile['token'])
-        } else {
-            // TODO throw error here
-            throw new HTTPException("Unauthorized, please authenticate first.", 401, array(
-                'dev' => "Must be authenticated to access.",
-                'internalCode' => '30945680384502037'
-            ));
+        switch ($config['security']) {
+            case true:
+                $token = $this->request->getHeader("X_AUTHORIZATION");
+                $token = trim(str_ireplace("Token: ", '', $token));
+                if (strlen($token) < 30) {
+                    throw new HTTPException("Bad token supplied", 401, array(
+                        'internalCode' => '0273497957'
+                    ));
+                }
+                
+                // TODO Check for a valid session
+                // Check if the variable is defined
+                if ($this->auth->isLoggedIn($token)) {
+                    parent::__construct($parseQueryString);
+                } else {
+                    // TODO throw error here
+                    throw new HTTPException("Unauthorized, please authenticate first.", 401, array(
+                        'dev' => "Must be authenticated to access.",
+                        'internalCode' => '30945680384502037'
+                    ));
+                }
+                break;
+            
+            case false:
+                parent::__construct($parseQueryString);
+                break;
+            
+            default:
+                throw new HTTPException("Bad security value supplied", 500, array(
+                    'internalCode' => '280273409724075'
+                ));
+                break;
         }
     }
 }
