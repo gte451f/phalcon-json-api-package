e9ddfde794e478d3426e4e2a3f6a58ff971e3dfe
Jim <jim.jenkins.iv@gmail.com>
2016-06-15 21:53:29 -0400

    add support for relationship away data objects

--- src/Result/Result.php
+++ src/Result/Result.php
@@ -1,6 +1,7 @@
 <?php
 namespace PhalconRest\Result;
 
+use \PhalconRest\Exception\HTTPException;
 use PhalconRest\Result\Data;
 
 /**
@@ -17,6 +18,10 @@ class Result extends \Phalcon\DI\Injectable
     // store a collection of data like items
     private $included = [];
 
+    // store the list of relationships for a "main" record type
+    // each data object will refer to this when formatting data objects
+    private $relationshipRegistry = [];
+
     /**
      * @var string is the result going to output a single result or an array of results?
      * single | multiple | error
@@ -29,6 +34,63 @@ class Result extends \Phalcon\DI\Injectable
         $this->setDI($di);
     }
 
+
+    /**
+     * write or overwrite a definition
+     * each definition should map to a table name
+     * we'll massage the name a little bit to cut down on errors
+     *
+     * // 1 = hasOne 0 = belongsTo 2 = hasMany
+     *
+     * @throws HTTPException
+     * @param $name
+     * @param $type
+     */
+    public function registerRelationshipDefinitions($relation, $type)
+    {
+        //convert to something that makes sense :)
+        switch ($type) {
+            case 0:
+                $type = 'hasOne';
+                $name = $relation->getTableName('singular');
+                break;
+            case 1:
+                $type = 'belongsTo';
+                $name = $relation->getTableName('singular');
+                break;
+            case 2:
+                $type = 'hasMany';
+                $name = $relation->getTableName('plural');
+                break;
+            case 3:
+                // not sure about this one
+                $type = 'hasManyToMany';
+                $name = $relation->getTableName('plural');
+                break;
+            default:
+                // throw error for bad type
+                throw new HTTPException("A Bad Relationship Type was supplied!", 500, array(
+                    'code' => '8948616164789797'
+                ));
+                break;
+        }
+        $this->relationshipRegistry[strtolower($name)] = $type;
+    }
+
+    /**
+     * simple getter
+     *
+     * @param $name
+     * @return array
+     */
+    public function getRelationshipDefinition($name)
+    {
+        if (isset($this->relationshipRegistry[$name])) {
+            return $this->relationshipRegistry[$name];
+        } else {
+            return 'Unknown';
+        }
+    }
 
     public function addData(Data $newData)
     {
