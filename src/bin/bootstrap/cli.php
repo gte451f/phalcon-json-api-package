9d64871699984ae30b1189015c65619d43cc7e10
Jim <jim.jenkins.iv@gmail.com>
2016-10-15 12:40:59 -0400

    fix bootstrap

---
+++ src/bin/bootstrap/cli.php
@@ -0,0 +1,24 @@
+<?php
+use Phalcon\CLI\Console as ConsoleApp;
+
+// Create a console application
+$console = new ConsoleApp();
+$console->setDI($di);
+
+/**
+ * Process the console arguments
+ */
+$arguments = [];
+foreach ($argv as $k => $arg) {
+    if ($k == 1) {
+        $arguments['task'] = $arg;
+    } elseif ($k == 2) {
+        $arguments['action'] = $arg;
+    } elseif ($k >= 3) {
+        $arguments['params'][] = $arg;
+    }
+}
+
+// define global constants for the current task and action
+define('CURRENT_TASK', (isset($argv[1]) ? $argv[1] : null));
+define('CURRENT_ACTION', (isset($argv[2]) ? $argv[2] : null));
