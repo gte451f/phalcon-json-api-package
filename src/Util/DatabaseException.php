18c8a3d0a96d917c7456ba0bbce8d6f449318583
Jim <jim.jenkins.iv@gmail.com>
2016-10-31 09:23:10 -0400

    correct bad reference in db exception

--- src/Util/DatabaseException.php
+++ src/Util/DatabaseException.php
@@ -1,9 +1,14 @@
 <?php
 namespace PhalconRest\Util;
 
+
+use PhalconRest\Util\HTTPException;
+
 /**
  * where caught Database Exceptions go to die, based on HTTP Exceptions
  *
  * @author jjenkins
  */
-class DatabaseException extends \HttpException { }
+class DatabaseException extends HttpException
+{
+}
